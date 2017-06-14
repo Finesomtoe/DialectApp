ec9ff205066346ce6d58bb298d6c64b4ec85dcc2
Finesomtoe <senendu5@yahoo.com>
2017-06-14 16:46:50 +0200

    Runserver update

--- DialectApp/runserver.py
+++ DialectApp/runserver.py
@@ -6,9 +6,9 @@ from os import environ
 from DialectApp import app
 
 if __name__ == '__main__':
-    #HOST = environ.get('SERVER_HOST', 'localhost')
-    #try:
-    #    PORT = int(environ.get('SERVER_PORT', '2424'))
-    #except ValueError:
-    #    PORT = 2424
-    app.run(host='localhost', port=2424)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        PORT = 5000
+    app.run(HOST, PORT)
